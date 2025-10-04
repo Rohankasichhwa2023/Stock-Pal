00a19646950a79824e36754bb1fb6b093667f9af
Rohan Kasichhwa <rohankasichhwa@gmail.com>
2025-10-04 12:24:05 +0545

    initial commit

---
+++ backend/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.TextField()
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
