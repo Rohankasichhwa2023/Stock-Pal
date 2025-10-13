1352f884c01d54782fc17cb10d07f6ad838c499d
Rohan Kasichhwa <rohankasichhwa@gmail.com>
2025-10-13 17:46:46 +0545

    signup problem solved

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,14 +1,13 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
+class User(models.Model):
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(max_length=100, unique=True)
-    password = models.TextField()
+    password = models.TextField()  # hashed password
     is_admin = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    REQUIRED_FIELDS = ["email"] 
 
     def __str__(self):
         return self.username
+
